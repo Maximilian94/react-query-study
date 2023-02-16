7ab6ac68fdea1ce5a00adf8727eb82817e51ff9c
Maximilian94 <mkadenfilgo@gmail.com>
2023-02-16 17:08:17 -0300

    react query installed and example created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from 'react-query';
+
+function fetchUser(username: string) {
+	return fetch(`https://api.github.com/users/${username}`).then((res) =>
+		res.json()
+	);
+}
+
+function GithubUser({ username }: { username: string }) {
+	const userQuery = useQuery([username], () => fetchUser(username));
+	const data = userQuery.data;
+
+	if (userQuery.isLoading) return <p>Loading...</p>;
+
+	if (userQuery.error) return <p>Error</p>;
+
+	return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<GithubUser username='Maximilian94' />
+		</div>
+	);
 }
 
 export default App;
