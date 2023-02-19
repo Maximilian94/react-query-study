acf48ffbc384847038fa7b5dd9cffe46e7074401
Maximilian94 <mkadenfilgo@gmail.com>
2023-02-18 21:21:09 -0300

    react query dev tools

--- src/helpers/useUserData.ts
+++ src/helpers/useUserData.ts
@@ -4,7 +4,7 @@ export const useUserData = (userId: string) => {
 	const usersData = useQuery(
 		['users', userId],
 		() => fetch(`/api/users/${userId}`).then((res) => res.json()),
-		{ enabled: !!userId }
+		{ enabled: !!userId, staleTime: 1000 * 60 * 5 }
 	);
 	return usersData;
 };
